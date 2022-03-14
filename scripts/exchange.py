e1180ebe5bab29d4a768908fe475ccd6f00953c5
Angelica <Angelica137@users.noreply.github.com>
2022-03-14 22:22:47 +0000

    add exchangeable_value(), green

--- scripts/exchange.py
+++ scripts/exchange.py
@@ -1,8 +1,8 @@
 def exchange_money(budget, exchange_rate):
     """
     :param budget: float - amount of money to exchange
-    :param exchange_value: float - amount of domestic 
-    currency equal to one unit of foreign currency
+    :param exchange_rate: float - amount of domestic currency 
+    equal to one unit of foreign currency
     :return: float - the budget's value in the foreign currency
     """
     return budget / exchange_rate
@@ -11,8 +11,8 @@ def exchange_money(budget, exchange_rate):
 def get_change(budget, exchange_value):
     """
     :param budget: float - amount of money before exhange
-    :param exchanging_value: float - the amount tha tis taken from the budget
-    to be exhcanged
+    :param exchange_value: float - the amount that is taken 
+    from the budget to be exhcanged
     :return: float - the amount of money that is left from the bodget
     """
     return budget - exchange_value
@@ -34,3 +34,20 @@ def get_number_of_bills(budget, denomination):
     :return: the number of new currency bills that are received
     """
     return budget // denomination
+
+
+def exchangeable_value(budget, exchange_rate, spread, denomination):
+    """
+    :param budget: float - amount of money to be exchanged
+    :param exchange_rate: float - amount of domestic currency 
+    equal to one unit of foreign currency
+    :param spread: int - the percentage taken as an exchange fee 
+    expressed as an integer
+    :param denomination: int - the value of a single bill
+    :return: the maximum value of the new currency in bills only
+    => removes the petty change
+    """
+    true_rate = exchange_rate + (spread / 100)
+    exchange_value = exchange_money(budget, true_rate)
+    number_of_bills = get_number_of_bills(exchange_value, denomination)
+    return number_of_bills * denomination
--- tests/test_exchange.py
+++ tests/test_exchange.py
@@ -17,9 +17,9 @@ def test_get_number_of_bills():
     assert get_number_of_bills(127.5, 5) == 25
 
 
-def test_exchange_value_returns_80():
-    assert exchange_value(127.25, 1.20, 10, 20) == 80
+def test_exchangeable_value_returns_80():
+    assert exchangeable_value(127.25, 1.20, 10, 20) == 80
 
 
 def test_exchange_value_returns_95():
-    assert exchange_value(127.25, 1.20, 10, 5) == 95
+    assert exchangeable_value(127.25, 1.20, 10, 5) == 95
